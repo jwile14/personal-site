8773e2450577af36e1cd8f71bc793fd570e26dbc
Ubuntu <ubuntu@ip-172-31-37-225.us-east-2.compute.internal>
2020-11-18 15:50:40 +0000

    Add apache startup functionality

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,4 @@
-sudo cp -a src/. /var/www/jwile/
+sudo apt-get update > /dev/null
+sudo apt-get install apache2 -y > /dev/null
+sudo service apache2 start > /dev/null
+sudo cp -a src/. /var/www/html/
